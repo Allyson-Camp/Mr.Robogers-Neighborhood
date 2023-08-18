13795b12675bc2f9e3de904c64c4a375787d268c
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-18 09:55:52 -0700

    numberCounter() test 2 js now passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
 function numberCounter(number) {
   let array = [];
-  if (number.toString().includes(1)) {
-    return "beep!"
-  } else {
-    return number;
-  }
-  array.push(number);
+   if (number.toString().includes(1)) {
+        array.push("Beep!");
+   } else {
+        array.push(number);
+   }
   return array;
 }
-
-//  array.push(number.toString());
