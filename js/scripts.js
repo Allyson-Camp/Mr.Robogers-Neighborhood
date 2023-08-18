ee27f68861734471016b2566a9cfea05c529c73d
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-18 10:52:53 -0700

    add js for numberCounter test #4, passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 function numberCounter(number) {
   let array = [];
   let userEntry = number.toString();
-  if (userEntry.includes(2)) {
+  if (userEntry.includes(3)) {
+    array.push("Won't you be my neighbor?");
+  } else if (userEntry.includes(2)) {
     array.push("Boop!");
   } else if (userEntry.includes(1)) {
-    array.push("Beep!");
-  } else {
+    array.push("Beep!")
+  }
+  else {
         array.push(userEntry);
    }
   return array;
