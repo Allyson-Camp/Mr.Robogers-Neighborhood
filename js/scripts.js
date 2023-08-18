7a5f905fedc5fa2455a8a7d72e9855029524b28c
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-18 11:54:44 -0700

    WIP js for numberToWord() test #5

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 function numberToWord(number) {
-  let array = [];
+  const array = [];
   let userEntry = number.toString();
-  if (userEntry.includes(3)) {
-    array.push("Won't you be my neighbor?");
-  } else if (userEntry.includes(2)) {
-    array.push("Boop!");
-  } else if (userEntry.includes(1)) {
-    array.push("Beep!")
-  }
-  else {
-        array.push(userEntry);
-   }
-  return array;
+      //branching
+      if (userEntry.includes(3)) {
+          array.push("Won't you be my neighbor?");
+      } else if (userEntry.includes(2)) {
+          array.push("Boop!");
+      } else if (userEntry.includes(1)) {
+          array.push("Beep!");
+      } else {
+          array.push(userEntry);
+      }
+      return array;
+      //branching
 }
+
+// for (let index = 0; index < array.length; index += 1)
+// for (let i = 0; i < array.length; i += userEntry) 
+
+// const newArray = array.map(function (element)
+// {
+//   return newArray;
+// })
+
+// array.forEach(function (number) {
+  
+// })
