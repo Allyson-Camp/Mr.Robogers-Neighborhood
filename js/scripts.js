d6b9361a6750872a061cc8d7c961421072918441
Allyson Camp <Allysoncamp92@gmail.com>
2023-08-18 13:28:12 -0700

    WIP js numberToWord() test 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 function numberToWord(number) {
   const array = [];
   let userEntry = number.toString();
-      //branching
-      if (userEntry.includes(3)) {
-          array.push("Won't you be my neighbor?");
-      } else if (userEntry.includes(2)) {
-          array.push("Boop!");
-      } else if (userEntry.includes(1)) {
-          array.push("Beep!");
-      } else {
-          array.push(userEntry);
-      }
-      return array;
-      //branching
-}
+        if (userEntry.includes(3)) {
+            array.push("Won't you be my neighbor?");
+        } else if (userEntry.includes(2)) {
+            array.push("Boop!");
+        } else if (userEntry.includes(1)) {
+            array.push("Beep!");
+        } else {
+            array.push(userEntry);
+        }
+        return array;
+    }
+
+
+
+
 
 // for (let index = 0; index < array.length; index += 1)
 // for (let i = 0; i < array.length; i += userEntry) 
@@ -26,3 +28,4 @@ function numberToWord(number) {
 // array.forEach(function (number) {
   
 // })
+
